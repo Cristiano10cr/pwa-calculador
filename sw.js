1053fc09fa43e42904a3c1b39fb9a8d9c8450892
Cristiano10cr <167020718+Cristiano10cr@users.noreply.github.com>
2025-09-12 19:51:22 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,20 @@
-const CACHE_NAME = "calculadora-v1";
-const URLS_A_CACHE = [
-  "/Calculadora-PWA/",
-  "/Calculadora-PWA/index.html",
-  "/Calculadora-PWA/icon-192.png",
-  "/Calculadora-PWA/icon-512.png"
-];
-
-// Instalar Service Worker y guardar en caché
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(URLS_A_CACHE);
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("peces-v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icono.png"
+      ]);
     })
   );
 });
 
-// Activar Service Worker y limpiar cachés viejas
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-});
-
-// Interceptar peticiones y responder desde caché
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
